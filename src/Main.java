dfe78773cc1324813548b36c9a49716ef57b557b
ElenaKr77 <elena.kriventsova77@gmail.com>
2025-04-27 23:08:22 +0300

    src

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double weight = 98;
+        double height = 1.87;
+        double index = service.calculate(weight, height);
+        System.out.println((int) index);
+    }
 }
+
